be4ab23c84626efca59355f570ebc7a8b6031643
Sergiu Starciuc <ciukstar@yahoo.com>
2017-02-17 00:17:22 +0300

    Done Exercise 2.25. Continue with Exercise 2.26 at page 183

--- sicp/src/main/trees.scm
+++ sicp/src/main/trees.scm
@@ -5,12 +5,13 @@
         (else (+ (count-leaves (car x))
                  (count-leaves (cdr x))))))
 
-;; (count-leaves (cons (list 1 2) (list 3 4)))
-;; (count-leaves (cons (cons 1 (cons 2 ())) (cons 3 (cons 4 ()))))
-;; (count-leaves (+ (count-leaves (cons 1 (cons 2 ()))) (count-leaves (cons 3 (cons 4 ())))))
-;; (count-leaves (+ (count-leaves (+ (count-leaves 1) (count-leaves (cons 2 ())))) (count-leaves (+ (count-leaves 3) (count-leaves (cons 4 ()))))))
-;; (count-leaves (+ (count-leaves (+ 1 (count-leaves (+ (count-leaves 2) (count-leaves ()))))) (count-leaves (+ 1 (count-leaves (+ (count-leaves 4) (count-leaves ())))))))
-;; (count-leaves (+ (count-leaves (+ 1 (count-leaves (+ 1 0)))) (count-leaves (+ 1 (count-leaves (+ 1 0))))))
-;; (count-leaves (+ (count-leaves 2) (count-leaves 2)))
-;; (count-leaves (+ (count-leaves 2) (count-leaves 2)))
+;; (cnt (cons (list 1 2) (list 3 4)))
+;; (cnt (cons (cons 1 (cons 2 ())) (cons 3 (cons 4 ()))))
+;; (+ (cnt (cons 1 (cons 2 ()))) (cnt (cons 3 (cons 4 ()))))
+;; (+ (+ (cnt 1) (cnt (cons 2 ()))) (+ (cnt 3) (cnt (cons 4 ()))))
+;; (+ (+ 1 (+ (cnt 2) (cnt ()))) (+ 1 (+ (cnt 4) (cnt ()))))
+;; (+ (+ 1 (+ 1 0)) (+ 1 (+ 1 0)))
+;; (+ (+ 1 1) (+ 1 1))
+;; (+ 2 2)
+;; 4
 
