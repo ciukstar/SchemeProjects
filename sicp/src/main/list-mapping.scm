fe3574b4368ecf2e3886d059b15e7024770706df
Sergiu Starciuc <ciukstar@yahoo.com>
2017-02-16 16:28:39 +0300

    Done with lists

--- sicp/src/main/list-mapping.scm
+++ sicp/src/main/list-mapping.scm
@@ -1,4 +1,66 @@
 
+(define (for-each proc items)
+  (if (null? items) #t
+      (if ((lambda (x) #t) (proc (car items)))
+       (for-each proc (cdr items)))))
+
+;; (for-each display (list 1 2 3 4))
+;; (display 1) (for-each diplay (list 2 3 4))
+
+(define (square-list-iter-reverse2 items)
+  (define (iter things answer)
+    (if (null? things) answer
+        (iter (cdr things) (cons answer (square (car things))))))
+  (iter items ()))
+
+;; (square-list-iter-reverse (list 1 2 3 4))
+;; (iter (list 1 2 3 4) ())
+;; (iter (list 2 3 4) (cons () (square 1)))
+;; (iter (list 3 4) (cons (cons () (square 1)) (square 2)))
+;; (iter (list 4) (cons (cons (cons () (square 1)) (square 2)) (square 3)))
+;; (iter () (cons (cons (cons (cons () (square 1)) (square 2)) (square 3)) (square 4)))
+;; (cons (cons (cons (cons () (square 1)) (square 2)) (square 3)) (square 4))
+
+(define (square-list-iter-reverse items)
+  (define (iter things answer)
+    (if (null? things) answer
+        (iter (cdr things) (cons (square (car things)) answer))))
+  (iter items ()))
+
+;; (square-list-iter (list 1 2 3 4))
+;; (iter (list 1 2 3 4) ())
+;; (iter (list 2 3 4) (cons (square 1) ()))
+;; (iter (list 3 4) (cons (square 2)) (cons (square 1) ()))
+;; (iter (list 4) (cons (square 3) (cons (square 2)) (cons (square 1) ())))
+;; (iter () (cons (square 4) (cons (square 3) (cons (square 2)) (cons (square 1) ()))))
+;; (cons (square 4) (cons (square 3) (cons (square 2)) (cons (square 1) ())))
+;; (list 16 9 2 1)
+
+(define (square-list l)
+  (map square l))
+
+;; (square-list (list 1 2 3 4))
+;; (map square (list 1 2 3 4))
+;;; (cons (square 1) (map square (list 2 3 4)))
+;; (cons (square 1) (cons (square 2) (map square (list 3 4))))
+;; (cons (square 1) (cons (square 2) (cons (square 3) (map square (list 4)))))
+;; (cons (square 1) (cons (square 2) (cons (square 3) (cons (square 4) (map square ())))))
+;; (cons (square 1) (cons (square 2) (cons (square 3) (cons (square 4) ()))))
+
+
+(define (square-list0 l)
+  (if (null? l) ()
+      (cons (square (car l)) (square-list0 (cdr l)))))
+
+;; (square-list0 (list 1 2 3 4))
+;; (cons (square 1) (square-list0 (list 2 3 4)))
+;; (cons (square 1) (cons (square 2) (square-list0 (list 3 4))))
+;; (cons (square 1) (cons (square 2) (cons (square 3) (square-list0 (list 4)))))
+;; (cons (square 1) (cons (square 2) (cons (square 3) (cons (square 4) ()))))
+;; (cons 1 (cons 4 (cons 9 16)))
+;; (cons 1 (list 4 9 16))
+;; (list 1 4 9 16)
+
 (define (map proc items)
   (if (null? items) ()
       (cons (proc (car items)) (map proc (cdr items)))))
--- sicp/src/test/list-mapping-test.scm
+++ sicp/src/test/list-mapping-test.scm
@@ -1,6 +1,18 @@
 (load "lib/test-manager/load.scm")
 (load "src/main/list-mapping.scm")
 
+(define-test (square-list-iter-reverse-test)
+  (let ((l (list 1 2 3 4)))
+    (assert-true (equal? (square-list-iter-reverse l) (list 16 9 4 1)))))
+
+(define-test (square-list-test)
+  (let ((l (list 1 2 3 4)))
+    (assert-true (equal? (square-list l) (list 1 4 9 16)))))
+
+(define-test (square-list0-test)
+  (let ((l (list 1 2 3 4)))
+    (assert-true (equal? (square-list0 l) (list 1 4 9 16)))))
+
 (define-test (increment-test)
   (let ((l1 (map (lambda (x) (+ x 1)) (list 1 2 3)))
         (l2 (map abs (list -10 2.5 11.6 17))))
